a0f40e9cfd8b98a4620e663b0dea6b266eb28f92
courtamour <novikova19992806@gmail.com>
2023-02-12 14:00:30 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
-// You should implement your task here.
+module.exports = function towelSort(matrix) {
+  let result = [];
+  if (!matrix){
+    return result;
+  }
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+  else{
+  matrix.map((massiv, index) => {
+    if (index % 2 == 0 || index == 0) {
+      return result.push(massiv.filter(element => !(element == ',')));
+    }
+    else { return result.push(massiv.filter(element => !(element == ',')).reverse()); }
+  });
+  result = result.flat();
+  
+  return result;
+  }}
+
